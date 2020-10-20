a1f92fa91b41334632d32c8993b08375606302d9
kunal prakash <kunalprakash1309@gmail.com>
2020-10-21 01:08:38 +0530

    security review

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,19 @@
-const handleImage = (req, res) => {
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: 'abf02afd47ae423d8e7f6eb860ab7956'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with API"))
+}
+
+const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id','=', id)
     .increment('entries', 1)
@@ -10,5 +25,6 @@ const handleImage = (req, res) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 }
